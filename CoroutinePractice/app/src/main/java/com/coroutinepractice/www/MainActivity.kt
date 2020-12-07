23006bf2b59081229958666909d0338dcf47d186
DJDrama <putzzang@hotmail.com>
2020-12-07 17:26:17 +0900

    Async and Await

--- CoroutinePractice/app/src/main/java/com/coroutinepractice/www/MainActivity.kt
+++ CoroutinePractice/app/src/main/java/com/coroutinepractice/www/MainActivity.kt
@@ -7,6 +7,7 @@ import com.coroutinepractice.www.databinding.ActivityMainBinding
 import kotlinx.coroutines.*
 import kotlinx.coroutines.Dispatchers.Default
 import kotlinx.coroutines.Dispatchers.IO
+import kotlin.system.measureTimeMillis
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -16,36 +17,28 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val job = GlobalScope.launch(Default){
-            Log.d(TAG, "Starting long running calculation...")
-            withTimeout(300L){ // we can use this  as job.cancel()
-                for(i in 30..40){
-                    if(isActive) { // use it for cancellation
-                        val result = fib(i)
-                        Log.d(TAG, "Result for i = $i: $result")
-                    }
+        GlobalScope.launch(IO) {
+            val time = measureTimeMillis {
+                val answer1 = async{
+                    networkCall1()
                 }
-                Log.d(TAG, "Ending long running calculator...")
+                val answer2 = async{
+                    networkCall2()
+                }
+                Log.d(TAG, "Answer1 is ${answer1.await()}")
+                Log.d(TAG, "Answer2 is ${answer2.await()}")
             }
-//            repeat(5){
-//                Log.d(TAG, "Coroutine is still working...")
-//                delay(1000L)
-//            }
+            Log.d(TAG, "Requests took $time ms.")
         }
-//
-//        runBlocking {
-//            delay(500L)
-//            job.cancel() // after 2 seconds cancels the job.
-//            //job.join() // blocks the main thread until this coroutine gets finished
-//           // Log.d(TAG, "Main Thread is continuing...")
-//            Log.d(TAG, "Canceled job!")
-//        }
     }
 
-    fun fib(n: Int): Long{
-        if(n==0) return 0
-        if(n==1) return 1
-        return fib(n-1)+fib(n-2)
+    suspend fun networkCall1(): String {
+        delay(3000L)
+        return "Answer 1"
     }
 
+    suspend fun networkCall2(): String {
+        delay(3000L)
+        return "Answer 2"
+    }
 }
