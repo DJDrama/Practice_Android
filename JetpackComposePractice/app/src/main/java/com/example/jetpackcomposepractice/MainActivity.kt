36f9c90f2d8fd0f604fdbc094efd549abbc2bde8
DJDrama <putzzang@hotmail.com>
2020-12-28 12:41:55 +0900

    Jetpack Compose Setup and MainActivity.kt Sample Test

--- JetpackComposePractice/app/src/main/java/com/example/jetpackcomposepractice/MainActivity.kt
+++ JetpackComposePractice/app/src/main/java/com/example/jetpackcomposepractice/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.jetpackcomposepractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setContent{
+            Column(
+                modifier = Modifier.padding(16.dp)
+            ){
+                Text(text="Hello I am DJ")
+                Spacer(Modifier.padding(top=10.dp))
+                Button(onClick = {}){
+                    Text(text = "This is a Button!")
+                }
+            }
+        }
+
     }
 }
