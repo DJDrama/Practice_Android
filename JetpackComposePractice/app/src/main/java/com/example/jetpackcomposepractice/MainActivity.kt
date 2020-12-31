69f82aad1d15ca6e18935aafcbf0ead2e59c1567
DJDrama <putzzang@hotmail.com>
2020-12-31 12:59:34 +0900

    Setting up Hilt and Test

--- JetpackComposePractice/app/src/main/java/com/example/jetpackcomposepractice/MainActivity.kt
+++ JetpackComposePractice/app/src/main/java/com/example/jetpackcomposepractice/MainActivity.kt
@@ -2,12 +2,26 @@ package com.example.jetpackcomposepractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    /** Field Injection **/
+    @Inject
+    lateinit var str: String
+
+    @Inject
+    lateinit var app: BaseApplication
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Log.e("Test", "String: $str")
+        Log.e("Test", "App: $app")
 
     }
 }
