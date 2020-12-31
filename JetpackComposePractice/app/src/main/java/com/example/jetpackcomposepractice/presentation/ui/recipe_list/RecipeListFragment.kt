703376b045797634178c27a867b109129423b770
DJDrama <putzzang@hotmail.com>
2020-12-31 14:16:39 +0900

    RecyclerView + CardView Using Composable

--- JetpackComposePractice/app/src/main/java/com/example/jetpackcomposepractice/presentation/ui/recipe_list/RecipeListFragment.kt
+++ JetpackComposePractice/app/src/main/java/com/example/jetpackcomposepractice/presentation/ui/recipe_list/RecipeListFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
@@ -19,6 +20,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
 import com.example.jetpackcomposepractice.R
+import com.example.jetpackcomposepractice.presentation.components.RecipeCard
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -34,23 +36,15 @@ class RecipeListFragment : Fragment() {
     ): View {
         return ComposeView(requireContext()).apply {
             setContent {
-                val recipes = viewModel.recipes.value
-                for(recipe in recipes){
-                    Log.e("RecipeListFragment", "Check : $recipe")
-                }
-
-                Column(modifier = Modifier.padding(16.dp)) {
-                    Text(
-                        text = "Recipe List",
-                        style = TextStyle(fontSize = TextUnit.Companion.Sp(21))
-                    )
-                    Spacer(modifier = Modifier.padding(10.dp))
-                    Button(onClick = {
-                        findNavController().navigate(R.id.action_recipeListFragment_to_recipeFragment)
-                    }) {
-                        Text("To Recipe Fragment")
+                val recipes = viewModel.recipes.value // observable ds
+                LazyColumn {
+                    itemsIndexed(items = recipes) { index, recipe ->
+                        RecipeCard(recipe = recipe) {
+                            Log.e("Test", "Clicked : $recipe")
+                        }
                     }
                 }
+
             }
         }
     }
