63dd5992842d3754a4ae2e7001730e2d39847599
DJDrama <putzzang@naver.com>
2021-02-18 21:46:10 +0900

    Providing the Room Database with Hilt

---
+++ JetpackComposePractice/app/src/main/java/com/example/jetpackcomposepractice/di/CacheModule.kt
@@ -0,0 +1,32 @@
+package com.example.jetpackcomposepractice.di
+
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.jetpackcomposepractice.cache.RecipeDao
+import com.example.jetpackcomposepractice.cache.database.AppDatabase
+import com.example.jetpackcomposepractice.cache.database.AppDatabase.Companion.DATABASE_NAME
+import com.example.jetpackcomposepractice.presentation.BaseApplication
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object CacheModule {
+
+    @Singleton
+    @Provides
+    fun provideRoomDatabase(app: BaseApplication): AppDatabase {
+        return Room.databaseBuilder(app, AppDatabase::class.java, DATABASE_NAME)
+            .fallbackToDestructiveMigration()
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRecipeDao(app: AppDatabase): RecipeDao{
+        return app.recipeDao()
+    }
+}
