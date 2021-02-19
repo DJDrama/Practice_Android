2fae68aeeb035d76e969c459f4e479554b079e6b
DJDrama <putzzang@naver.com>
2021-02-19 23:07:33 +0900

    InteractorsModule.kt, RecipeListViewModel.kt providing use cases, Flow

---
+++ JetpackComposePractice/app/src/main/java/com/example/jetpackcomposepractice/di/InteractorsModule.kt
@@ -0,0 +1,30 @@
+package com.example.jetpackcomposepractice.di
+
+import com.example.jetpackcomposepractice.cache.RecipeDao
+import com.example.jetpackcomposepractice.cache.model.RecipeEntityMapper
+import com.example.jetpackcomposepractice.interactors.recipe_list.SearchRecipes
+import com.example.jetpackcomposepractice.network.RecipeService
+import com.example.jetpackcomposepractice.network.model.RecipeDtoMapper
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.scopes.ViewModelScoped
+
+@Module
+@InstallIn(ViewModelComponent::class) // Only exists in ViewModels
+object InteractorsModule {
+
+    /** Use Cases **/
+
+    @ViewModelScoped
+    @Provides
+    fun provideSearchRecipes(
+        recipeDao: RecipeDao,
+        recipeService: RecipeService,
+        dtoMapper: RecipeDtoMapper,
+        entityMapper: RecipeEntityMapper
+    ): SearchRecipes {
+        return SearchRecipes(recipeDao, recipeService, entityMapper, dtoMapper)
+    }
+}
