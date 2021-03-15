10750dfadadf25170d2211b59c654ae91aa0b6cc
DJDrama <putzzang@naver.com>
2021-03-15 15:57:30 +0900

    Making SearchFragment and etc

--- MyMovieRecom/app/src/main/java/com/movierecom/www/ui/MainFragment.kt
+++ MyMovieRecom/app/src/main/java/com/movierecom/www/ui/MainFragment.kt
@@ -1,8 +1,35 @@
 package com.movierecom.www.ui
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.movierecom.www.R
+import com.movierecom.www.databinding.FragmentMainBinding
 
-class MainFragment: Fragment(R.layout.fragment_main) {
+class MainFragment : Fragment(R.layout.fragment_main), View.OnClickListener {
+    private var _binding: FragmentMainBinding? = null
+    private val binding: FragmentMainBinding
+        get() = _binding!!
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentMainBinding.bind(view)
+
+        binding.cardView2.setOnClickListener(this)
+        binding.editText.setOnClickListener(this)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding=null
+    }
+
+    override fun onClick(v: View?) {
+        when(v){
+            binding.cardView2, binding.editText->{
+                findNavController().navigate(R.id.action_mainFragment_to_searchFragment)
+            }
+        }
+    }
 }
