22c5107a2aacc506833666092733705589ca2544
DJDrama <putzzang@naver.com>
2021-09-02 23:26:49 +0900

    Setting up dto

--- Dota-Info-starting-point/hero/hero-datasource/build.gradle.kts
+++ Dota-Info-starting-point/hero/hero-datasource/build.gradle.kts
@@ -1,9 +1,16 @@
 apply {
     from("$rootDir/library-build.gradle")
 }
+plugins {
+    kotlin(KotlinPlugins.serialization) version Kotlin.version
+}
 
 dependencies {
     // dependencies which are not included in the parent build.gradle
     // but can be used inside "components" module
+    "implementation"(project(Modules.heroDomain))
 
+    "implementation"(Ktor.core)
+    "implementation"(Ktor.clientSerialization)
+    "implementation"(Ktor.android)
 }
