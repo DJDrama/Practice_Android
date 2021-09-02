89486eff19d4a1aefed8307592e401031f77d098
DJDrama <putzzang@naver.com>
2021-09-02 23:58:58 +0900

    Testing GetHeroes Usecase in MainActivity

--- Dota-Info-starting-point/app/src/main/java/com/dj/dotainfo/ui/MainActivity.kt
+++ Dota-Info-starting-point/app/src/main/java/com/dj/dotainfo/ui/MainActivity.kt
@@ -3,16 +3,75 @@ package com.dj.dotainfo.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.dj.core.DataState
+import com.dj.core.Logger
+import com.dj.core.ProgressBarState
+import com.dj.core.UIComponent
 import com.dj.dotainfo.ui.theme.DotaInfoTheme
+import com.dj.hero_domain.Hero
+import com.dj.hero_interactors.HeroInteractors
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
 class MainActivity : ComponentActivity() {
 
+    private val heroes: MutableState<List<Hero>> = mutableStateOf(emptyList())
+    private val progressBarState: MutableState<ProgressBarState> =
+        mutableStateOf(ProgressBarState.Idle)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val getHeroes = HeroInteractors.build().getHeroes
+        val logger = Logger("Get Heroes Test")
+
+        getHeroes.execute().onEach { dataState ->
+            when (dataState) {
+                is DataState.Response -> {
+                    when (dataState.uiComponent) {
+                        is UIComponent.Dialog -> {
+                            logger.log((dataState.uiComponent as UIComponent.Dialog).description)
+                        }
+                        is UIComponent.None -> {
+                            logger.log((dataState.uiComponent as UIComponent.None).message)
+                        }
+                    }
+                }
+                is DataState.Data -> {
+                    heroes.value = dataState.data ?: emptyList()
+                }
+                is DataState.Loading -> {
+                    progressBarState.value = dataState.progressBarState
+                }
+            }
+        }.launchIn(
+            CoroutineScope(IO)
+        )
+
         setContent {
             DotaInfoTheme {
-                Text("Starting point")
+                Box(modifier = Modifier.fillMaxSize()) {
+                    LazyColumn {
+                        items(heroes.value) { hero ->
+                            Text(text = hero.localizedName)
+                        }
+                    }
+                    if (progressBarState.value is ProgressBarState.Loading) {
+                        CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+                    }
+                }
             }
         }
     }
