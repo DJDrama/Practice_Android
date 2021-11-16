7f0be2a1e30ef0d426e2c125b8db1c6c7db80946
DJDrama <putzzang@naver.com>
2021-11-16 22:47:59 +0900

    Fake Network DataSource

--- Dota-Info-starting-point/hero/hero-datasource-test/build.gradle.kts
+++ Dota-Info-starting-point/hero/hero-datasource-test/build.gradle.kts
@@ -2,8 +2,16 @@ apply {
     from("$rootDir/library-build.gradle")
 }
 
+plugins {
+    kotlin(KotlinPlugins.serialization) version Kotlin.version
+}
+
 dependencies {
     // dependencies which are not included in the parent build.gradle
     // but can be used inside "components" module
+    "implementation"(project(Modules.heroDataSource))
+    "implementation"(project(Modules.heroDomain))
 
+    "implementation"(Ktor.ktorClientMock)
+    "implementation"(Ktor.clientSerialization)
 }
